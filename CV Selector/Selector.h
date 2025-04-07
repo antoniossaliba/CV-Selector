4a01a166e44bece1d19e2f37c76ee3fe4b6cb5cf
Antonios Saliba <168730127+antoniossaliba@users.noreply.github.com>
2025-04-07 08:17:39 +0300

    Add files via upload

---
+++ CV Selector/Selector.h
@@ -0,0 +1,27 @@
+#ifndef SELECTOR_H
+#define SELECTOR_H
+
+#include <iostream>
+#include <vector>
+#include "Applicant.h"
+using namespace std;
+
+class Selector
+{
+private:
+    vector<Applicant> applicants;
+    const string filename = "applicants.txt";
+
+public:
+    Selector() {}
+    void addApplicant(const Applicant &applicant);
+    void removeApplicant(const string &email);
+    void modifyApplicant(const string &email, const Applicant &updatedApplicant);
+    string searchApplicant(const string &email);
+    void displayApplicants() const;
+    void saveApplicants() const;
+    void loadApplicants();
+    int getNumberOfApplicants() const;
+};
+
+#endif
